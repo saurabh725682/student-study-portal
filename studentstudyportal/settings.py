edd7eb23ff92347e622a9549bc0c935415fb81ce
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-03-29 11:12:50 +0530

    Update settings.py

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+qp!u3-sb_mj)q3!240jwtq1tpbtec&zdkqjb_%qi789@4ye5z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
