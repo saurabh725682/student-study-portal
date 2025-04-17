3be24b007f36f8bb7fd453eeab9dce5cd4b81fa0
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-04-17 16:26:52 +0530

    Update forms.py

--- Authentication/forms.py
+++ Authentication/forms.py
@@ -2,15 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-
+# User Registratiion form..
 class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
         
-
-class LoginForm(AuthenticationForm):
-   
+# User login form..
+class LoginForm(AuthenticationForm): 
     username = forms.CharField(widget=forms.TextInput())
     password = forms.CharField(widget=forms.PasswordInput())
     
